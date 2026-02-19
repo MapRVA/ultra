fe2ba32ba65c302a50cf2b2fccffeb138bad029d
Daniel Schep <dschep@gmail.com>
2026-02-19 14:59:57 -0500

    maprva ultra!

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Overpass Ultra config
+# MapRVA Ultra config
 
-This repo contains the [Ultra](https://gitlab.com/trailstash/ultra) & GitLab Pages
-[config](./config.js) for [ohm.overpass-ultra.us](https://ohm.overpass-ultra.us)
+This repo contains custom [config](./config.js) for [Ultra](https://gitlab.com/trailstash/ultra).
+
+It customizes Ultra to:
+
+* Use MapRVA's Virginia-only Overpass Server: `overpass.maprva.org`
+* Use styles from MapRVA's style server: `styles.maprva.org`
--- config.mjs
+++ config.mjs
@@ -1,177 +1,122 @@
+import AutoProvider from "@trailstash/ultra/lib/queryProviders/auto.js";
 import overpass from "@trailstash/ultra/lib/queryProviders/overpass.js";
-import sparql from "@trailstash/ultra/lib/queryProviders/sparql.js";
+import { all } from "@trailstash/ultra/lib/queryProviders/index.js";
 
-const mapStyle = "https://www.openhistoricalmap.org/map-styles/main/main.json";
+const mapStyle = "https://styles.maprva.org/openmaptiles-osm.json";
 const query = `/*
 This is an example Overpass query.
 Try it out by pressing the Run button above!
 */
-nwr["amenity"="theatre"]["start_date"](if:
-  t["start_date"] < "1976" &&
-  (!is_tag("end_date") || t["end_date"] >= "1975"));
-
-out geom;
-`;
-
-const popupTemplate = `
-  <h2>
-    {{ type }}
-    <a href="https://openhistoricalmap.org/{{ type }}/{{ id }}" target="_blank">{{ id }}</a>
-    <a href="https://openhistoricalmap.org/edit?{{ type }}={{ id }}" target="_blank">✏️</a>
-  </h2>
-  <h3>Tags</h3>
-  {%- for tag in tags %}
-    {%- if tag[0] contains "website" %}
-      <code>{{ tag[0] }} = <a href="{{ tag[1] }}" target="_blank">{{ tag[1] }}</a></code>
-    {%- elsif tag[0] contains "wikidata" %}
-      <code>{{ tag[0] }} = <a href="https://wikidata.org/wiki/{{ tag[1] }}" target="_blank">{{ tag[1] }}</a></code>
-    {%- elsif tag[0] contains "wikipedia" %}
-      {% assign lang = tag[1] | split: ":" | first %}
-      <code>{{ tag[0] }} = <a href="https://{{ lang }}.wikipedia.org/wiki/{{ tag[1] | replace_first: lang, "" | replace_first: ":", "" }}" target="_blank">{{ tag[1] }}</a></code>
-    {%- else %}
-      <code>{{ tag[0] }} = {{ tag[1] }}</code>
-    {%- endif %}
-    <br>
-  {%- endfor %}
-  {%- if meta %}
-    <h3>Meta</h3>
-    {%- for tag in meta %}
-      {%- if tag[0] == "changeset" %}
-        <code>{{ tag[0] }} = <a href="https://openhistoricalmap.org/changeset/{{ tag[1] }}" target="_blank">{{ tag[1] }}</a></code>
-      {%- elsif tag[0] == "user" %}
-        <code>{{ tag[0] }} = <a href="https://openhistoricalmap.org/user/{{ tag[1] }}" target="_blank">{{ tag[1] }}</a></code>
-      {%- else %}
-        <code>{{ tag[0] }} = {{ tag[1] }}</code>
-      {%- endif %}
-        <br>
-    {%- endfor %}
-  {%- endif %}
-  {%- if coordinates %}
-    <h3>Coordinates</h3>
-    <a href="geo://{{coordinates[1]}},{{coordinates[0]}}">{{coordinates[1] | round: 6 }} / {{coordinates[0] | round: 6 }}</a> <small>(lat/lon)</small>
-  {%- endif %}
-`;
-
-const help = `
-# Introduction
-
-This is an [OpenHistoricalMap](https://openhistoricalmap.org)-specific deployment of
-[Ultra](https://gitlab.com/trailstash/ultra) a web-application made to simplify making maps with
-[MapLibre GL JS](https://maplibre.org). It can load [OpenHistoricalMap](https://openhistoricalmap.org) data
-from the [Overpass API](https://overpass-api.de/).
-
-## Documentation
-
-The documentation for Ultra is available at <a target="_blank" href="https://overpass-ultra.us/docs/">overpass-ultra.us/docs</a>
-
-## Overpass Queries
-
-The Overpass API allows you to query for OHM data by your own search criteria. For this purpose, it
-has a specifically crafted query language: [Overpass
-QL](https://wiki.openstreetmap.org/wiki/Overpass_API/Overpass_QL)
-
-In addition to regular Overpass API queries one can use \`{{bbox}}\` to specify the bounding box
-coordinates of the current map view.
-
-## Configuration
-
-Various aspects of Ultra, such as styling and the Overpass API server to use, can be
-configured via [YAML front-matter](https://overpass-ultra.us/docs/yaml).
-
-## Styling
-
-Ultra supports styling using an
-[extended](https://overpass-ultra.us/docs/style#ultra-maplibre-styles) version of the the [MapLibre
-Style Spec](https://maplibre.org/maplibre-style-spec/).
+[bbox:{{bbox}}];
+(
+way[highway=path];
+way[highway=footway];
+way[highway=cycleway];
+way[highway=steps];
+);
+out geom;`;
 
 
-See the [Styling](https://overpass-ultra.us/docs/style) section for more information.
-
-## About
-
-Ultra is built by Daniel Schep.
-
-You can contact me on [Mastodon](https://mapstodon.space/@trailstash).
-
-## Feedback, Bug Reports, Feature Requests
-
-While Ultra has been in development for a while, it should still be considered experimental.
-
-If you would like to report a bug or provide other feedback, please do so in the project's
-[Issue Tracker](https://gitlab.com/trailstash/ultra/-/issues).
-
-## Source Code</h4>
-
-The [source code](https://gitlab.com/trailstash/ultra) of this application is released under the
-[MIT license](https://gitlab.com/trailstash/overpass-ultra/-/blob/master/LICENSE).
-`;
-
-const settings = {
-  type: "overpass",
-  mapStyle,
-  queryProviders: {
-    overpass: {
-      ...overpass,
-      source: async function (query, controller, { server }) {
-        if (!server) {
-          server = "https://overpass-api.openhistoricalmap.org/api";
-        }
-        const src = await overpass.source(query, controller, { server });
-        delete src.attribution;
-        return src;
-      },
-      popupTemplate,
+const customAll = {
+  ...all,
+  overpass: {
+    ...overpass,
+    source: async function (query, controller, { server }) {
+      if (!server) {
+        server = "https://overpass.maprva.org/api";
+      }
+      return overpass.source(query, controller, { server });
     },
-    sparql,
   },
 };
+
+const settings = {
+  type: "auto",
+  mapStyle,
+  queryProviders: {
+    auto: new AutoProvider(customAll),
+    ...customAll,
+  },
+};
+
+const omtAttribution = '<a target="_blank" href="https://openmaptiles.org">OpenMapTiles</a> (Tiles by <a target="_blank" href="https://tiles.openstreetmap.us/">OSM US</a>)';
+const styles = [
+  [
+    `OSM OpenMapTiles`,
+    "https://styles.maprva.org/openmaptiles-osm.json",
+    omtAttribution,
+  ],
+  [
+    `OSM Liberty`,
+    "https://styles.maprva.org/osm-liberty.json",
+    omtAttribution,
+  ],
+  [
+    `OSM Bright`,
+    "https://styles.maprva.org/osm-bright.json",
+    omtAttribution,
+  ],
+  [
+    `Positron`,
+    "https://styles.maprva.org/positron.json",
+    omtAttribution,
+  ],
+  [
+    `Dark Matter`,
+    "https://styles.maprva.org/dark-matter.json",
+    omtAttribution,
+  ],
+  [
+    `MapTiler Basic`,
+    "https://styles.maprva.org/maptiler-basic.json",
+    omtAttribution,
+  ],
+  [
+    `MapTiler 3D`,
+    "https://styles.maprva.org/maptiler-3d.json",
+    omtAttribution,
+  ],
+  [
+    `Toner`,
+    "https://styles.maprva.org/maptiler-toner.json",
+    omtAttribution,
+  ],
+  [
+    `Fiord Color`,
+    "https://styles.maprva.org/fiord-color.json",
+    omtAttribution,
+  ],
+  [
+    `Virginia Base Mapping Program`,
+    "https://styles.maprva.org/vgin-vbmp-imagery-latest.json",
+    `Imagery`,
+  ],
+  [
+    `Henrico Aerial Imagery`,
+    "https://styles.maprva.org/henrico-aerial-imagery-latest.json",
+    `Imagery`,
+  ],
+  [
+    `Esri World Imagery`,
+    "https://styles.maprva.org/henrico-aerial-imagery-latest.json",
+    `Imagery`,
+  ],
+];
+
 
 export const defaultMode = "ide";
 export const modes = {
   ide: {
     query,
-    help,
-    styles: [
-      [
-        "Historic",
-        mapStyle,
-        '<a href="https://openhistoricalmap.org">OpenHistoricalMap</a>',
-      ],
-      [
-        "Rail",
-        "https://www.openhistoricalmap.org/map-styles/rail/rail.json",
-        '<a href="https://openhistoricalmap.org">OpenHistoricalMap</a>',
-      ],
-      [
-        "Woodblock",
-        "https://www.openhistoricalmap.org/map-styles/woodblock/woodblock.json",
-        '<a href="https://openhistoricalmap.org">OpenHistoricalMap</a>',
-      ],
-      [
-        "Japanese Scroll",
-        "https://www.openhistoricalmap.org/map-styles/japanese_scroll/ohm-japanese-scroll-map.json",
-        '<a href="https://openhistoricalmap.org">OpenHistoricalMap</a>',
-      ],
-      [
-        "OSM OpenMapTiles",
-        "https://styles.trailsta.sh/openmaptiles-osm.json",
-        '<a href="https://openfreemap.org">OpenFreeMap</a>',
-      ],
-    ],
+    styles,
     settings: {
       ...settings,
-      title: "OHM Ultra",
-      url: "https://ohm.overpass-ultra.us",
+      title: "MapRVA Ultra",
+      url: "https://ultra.maprva.org",
       options: {
-        zoom: 13,
-        center: [-73.9803, 40.7412],
         attributionControl: {
           customAttribution: "",
         },
-        maxBounds: [
-          [-179.999999999, -85.051129],
-          [179.999999999, 85.051129],
-        ],
       },
     },
   },
